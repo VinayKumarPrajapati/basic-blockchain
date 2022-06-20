0029b011752fe5c74333333056905cbd088d40ba
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-20 13:21:45 +0530

    created new transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 //Creating New Block 
@@ -9,17 +9,35 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transcations: this.newTransactions,
+        transcations: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
     //clearing out previous transcation to start again
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+    //Everytime new Transcation Created it will push newTransaction[] 
+    //This are pending Transactions 
+    //Validated after creating new block
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,9 +2,17 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(91098, '0IHUUBJ899M87','0NKI7878NHJJHJH');
-bitcoin.createNewBlock(91098, '0IHUU324J899M87','0NKI7878SDFSNHJJHJH');
-bitcoin.createNewBlock(91098, '0IHUUBJ8324299M87','0NKI7878SDFNHJJHJH');
 
+bitcoin.createNewTransaction(100,'VINAY0NKI7878NHJJHJHDE','PRAJA78NHJJHJHDE0NKI');
+
+bitcoin.createNewBlock(91098, '0IHUU324J899M87','0NKI7878SDFSNHJJHJH');
+
+//It will be pending till Creating new block
+bitcoin.createNewTransaction(100,'VINAY0NKI7878NHJJHJHDE','PRAJA78NHJJHJHDE0NKI');
+bitcoin.createNewTransaction(200,'VINAY0NKI7878NHJJHJHDE','PRAJA78NHJJHJHDE0NKI');
+bitcoin.createNewTransaction(300,'VINAY0NKI7878NHJJHJHDE','PRAJA78NHJJHJHDE0NKI');
+
+bitcoin.createNewBlock(91198, '0IHUU324J899M87','0NKI7878SDFSNHJJHJH');
 
 console.log(bitcoin);
 
