42676bd864947d05824104e241f9c390b2e282a8
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-20 19:19:50 +0530

    Created decentralized blockchain network

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -93,9 +93,18 @@ app.post('/register-node',function(req,res){
 });
 
 //register multiple nodes at once
-app.post('/register-nodes-bulk',function(req,res){
-
+app.post('/register-nodes-bulk', function(req, res){
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+        const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl)  == -1
+        const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+        if(nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(networkNodeUrl);
+    });
+    res.json({ note: 'Bulk Registration successful' });
 });
+
+
+
 
 app.listen(port, function() {
     console.log(`Listening to port ${port}`);
